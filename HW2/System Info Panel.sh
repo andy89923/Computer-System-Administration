a40af5e32e9586b74c2912bad1b0c54687ad2efb
CTFang <ctfang@ctfang-sa.cs.nctu.edu.tw>
2020-10-20 01:27:47 +0800

    Upload before 1020

--- HW2/System Info Panel.sh
+++ HW2/System Info Panel.sh
@@ -1,31 +1,43 @@
 # !/bin/sh
 
 LoginRank() {
-
 }
 
 PortInfo() {
+}
 
+MountPointInfo() {
+}
+
+SaveSystemInfo() {
+}
+
+LoadSystemInfo() {
 }
 
 Option() {
-    Selection=`cat choose.txt`
+    Selection=`cat choose.tmp`
     case $Selection in
         1)
-            LoginRank();
+            LoginRank;
             echo "Login Rank";;
         2)
-            PortInfo();
+            PortInfo;
             echo "Port Info";;
         3)
+            MountPointInfo;
             echo "MountPoint INFO";;
         4)
+            SaveSystemInfo;
             echo "Save System Info";;
         5)
+            LoadSystemInfo;
             echo "Load System Info";;
         *)
             echo "Error";;
     esac
+
+    rm choose.tmp
 }
 
 main() {
@@ -37,7 +49,7 @@ main() {
             "2" "PORT INFO" \
             "3" "MOUNTPOINT INFO" \
             "4" "SAVE SYSTEM INFO" \
-            "5" "LOAD SYSTEM INFO" 2>&1 > choose.txt
+            "5" "LOAD SYSTEM INFO" 2>&1 > choose.tmp
 
 
     case $? in
